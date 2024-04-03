3b11e2de82a9e95ececa623284d02a013b321c59
guorui <pdsgr1996@gmail.com>
2024-04-03 17:54:15 +0900

    feat: change pageId

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,20 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5e56dab6e29e4b36be280aabbc1038cb',
+  rootNotionPageId: '8606496368df447fb3dc643b90d42ed7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Jörmungandr‘s blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  author: 'Jörmungandr',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'valkyrja_rui',
+  github: 'restartgr',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
